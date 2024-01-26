0b28667ae842456d295c1858850db92a7f363ac0
Supratim Nag <138885948+snsupratim@users.noreply.github.com>
2024-01-26 21:49:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ var hours = Math.floor((time%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
 var minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)); 
 var seconds = Math.floor((time % (1000 * 60)) / 1000); 
 countdown.innerHTML = days + " Days : "  
-+ hours + " Hours :" + minutes + " Minutes :" + seconds + " Seconds :";
++ hours + " Hours :" + minutes + " Minutes : " + seconds + " Seconds";
 conhahsole.log(days + "Days :"  
-+ hours + " Hours :" + minutes + " Minutes :" + seconds + " Secodns :");
++ hours + " Hours :" + minutes + " Minutes : " + seconds + " Secodns");
 // check the time if its expired
     if (time < 0) { 
         clearInterval(x); 
